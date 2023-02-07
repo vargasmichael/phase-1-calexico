bcb12339edf9820f0f49e2c8a60573440847e616
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-02-07 10:45:58 -0700

    CC #1-3

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your code here...
+
+    fetch('http://localhost:3000/menu')
+    .then(function (response)  {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw (response.statusText)
+        }
+    })
+    .then(function(data) {
+            populateMenu(data)
+            populateMenuItems(data[0])
+        })
+
+const menu = document.querySelector('#menu-items');
+const image = document.querySelector('#dish-image');
+const name1 = document.querySelector('#dish-name');
+const description = document.querySelector('#dish-description');
+const price = document.querySelector('#dish-price');
+
+    function populateMenu(items){
+        items.forEach(item => {
+            const spanItem = document.createElement('span');
+            spanItem.innerText = item.name
+            menu.appendChild(spanItem)
+            spanItem.addEventListener('click', () => {
+                populateMenuItems(item)
+            } )
+        })
+    } console.log(menu)
+
+    function populateMenuItems(food){
+       image.src = food.image;
+       name1.innerText = food.name;
+       description.innerText = food.description;
+       price.innerText = food.price;
+    }
+
+    
+
+        
